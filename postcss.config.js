1473e8b0e4eb0a919110658b49e45ff38de693ed
utdsong <85722507+utdsong@users.noreply.github.com>
2025-01-11 14:26:54 +0700

    Add files via upload

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,6 @@
-const purgecss = require('@fullhuman/postcss-purgecss')
-
 module.exports = {
-  plugins: [
-    require('postcss-import'),
-    require('tailwindcss'),
-    require('autoprefixer'),
-    purgecss({
-      content: [
-        './app/**/*.html.erb',
-        './app/**/*.rb',
-        './app/javascript/**/*.js'
-      ],
-      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-    })
-  ]
-} 
+  plugins: {
+    'tailwindcss': {},
+    'autoprefixer': {},
+  }
+}
